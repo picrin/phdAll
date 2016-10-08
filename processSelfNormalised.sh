50dbe797ab8bcd4fc44b6c3a0af493875cfce78f
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-10-08 20:06:14 +0100

    Adding a report on discrepancies between Gene_level_expression.txt and my own analyses.

--- processSelfNormalised.sh
+++ processSelfNormalised.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 set -xev
-Rscript normalise.r
-mv CELfiles/blood/data.txt self-normalised.txt
+#Rscript normalise.r
+rm -f self-normalised.txt self-normalised2.txt
+cp analyses/$1 self-normalised.txt
 head -n 1 self-normalised.txt > self-normalised2.txt
 echo Affymetrix >> self-normalised2.txt
 tail -n +2 self-normalised.txt >> self-normalised2.txt
