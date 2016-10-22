75b086fca03b3b6386cad30273eab72f3b3808e9
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-10-22 14:56:29 +0100

    fix filename

--- analyseBlood.sh
+++ analyseBlood.sh
@@ -2,7 +2,6 @@
 set -e
 ./processBloodCELs.sh
 affymetrixFilename=normalised
-echo pass in a filename containing Affymetrix data you want analysed as the first argument.
 echo cleaning the dataset $affymetrixFilename.
 head $affymetrixFilename -n 1 > cleaned
 python cleanDatasets.py MASTERAnonymizedClinicalDataset23Feb2012.txt $affymetrixFilename DMBDI\ genotyping\ blood\ DNA\ Glasgow\ data\ with\ DMBDI\ ID\ for\ John.txt >> cleaned
@@ -13,5 +12,5 @@ echo repeated probes
 wc -l duplicates.same
 echo conflicting probes
 wc -l duplicates.different
-echo file normalised written to $PWD
-echo file containes $(wc -l normalised | awk '{print $1}') lines
+echo file cleaned written to $PWD
+echo file containes $(wc -l cleaned | awk '{print $1}') lines
