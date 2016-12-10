a9ba4c2a574ecbc5c5c1066cd8038c65ccc9d663
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-12-10 23:57:01 +0000

    reproducing Wilf's results

--- produceReport.sh
+++ produceReport.sh
@@ -1,6 +1,10 @@
+set -e
 jupyter nbconvert masterBlood.ipynb --to latex
 pdflatex masterBlood.tex
 jupyter nbconvert normalise.ipynb --to latex
 pdflatex normalise.tex
 jupyter nbconvert analyseLimma.ipynb --to latex
 pdflatex analyseLimma.tex
+cd overleaf
+pdflatex main.tex
+mv main.pdf ../litReview.pdf
