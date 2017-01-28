6659ed7deadc6242b5a4836ee7fdcd8fb598ec65
Adam Kurkiewicz <adam@kurkiewicz.pl>
2017-01-28 18:08:27 +0000

    running more experiments according to Simon's suggestions

--- produceReport.sh
+++ produceReport.sh
@@ -5,8 +5,8 @@ jupyter nbconvert normalise.ipynb --to latex
 pdflatex normalise.tex
 jupyter nbconvert analyseLimma.ipynb --to latex
 pdflatex analyseLimma.tex
-cd overleaf
-xelatex main
-biber main
-xelatex main
-mv main.pdf ../litReview.pdf
+#cd overleaf
+#xelatex main
+#biber main
+#xelatex main
+#mv main.pdf ../litReview.pdf
